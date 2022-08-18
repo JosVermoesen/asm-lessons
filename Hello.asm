f0ca46d484bf22eb394ddc207506613d8193babe
Jos Vermoesen <info@rv.be>
2022-08-18 21:46:17 +0200

    comments from GitHub CoPilot

--- Hello.asm
+++ Hello.asm
@@ -1,24 +1,24 @@
-PrintChar   equ &BB5A   
+PrintChar equ &BB5A                     ;reference to firmware print character function
 
-    org &1200
+    org &1200                           ;start of program    
 
-    ld hl,Message           ;Address of string
-    Call PrintString        ;Show String to screen
+    ld hl,Message                       ;load message into hl
+    Call PrintString                    ;print message
 
-    ret                     ;Finished Hello World
+    ret                                 ;return to firmware
 
 PrintString:
-    ld a,(hl)               ;Print a '255' terminated string
-    cp 255
-    ret z
-    inc hl
-    call PrintChar
-    jr PrintString
+    ld a,(hl)                           ;load one by one character from message
+    cp 255                              ;check for end of message
+    ret z                               ;return if end of message
+    inc hl                              ;increment message pointer
+    call PrintChar                      ;print each character
+    jr PrintString                      ;repeat until end of message
 
-Message: db 'Hello World 2022!',255
+Message: db 'Hello World 2022!',255     ;message to print
 
 NewLine:
-    ld a,13                 ;Carriage return
-    call PrintChar
-    ld a,10                 ;Line Feed
-    jp PrintChar
+    ld a,13                             ;load newline
+    call PrintChar                      ;print newline
+    ld a,10                             ;load line feed
+    jp PrintChar                        ;print line feed
